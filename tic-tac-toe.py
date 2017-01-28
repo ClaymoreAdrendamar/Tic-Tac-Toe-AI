0257f86f63549d540e02438bd917d3a37dffb9cb
Joseph Chotard <joseph@gamepickle.net>
2017-01-28 12:12:06 +0100

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,5 @@
 from environment import *
+import random
 from copy import deepcopy
 
 def player_move(board, text):
@@ -68,8 +69,56 @@ def play():
     else:
         print("It's a tie!")
 
+def minimax(node, player, alpha, beta):
+    if node.leaf():
+        if node.X_won():
+            return -1
+        elif node.tied():
+            return 0
+        elif node.O_won():
+            return 1
+
+    for move in node.legal_moves():
+        node.move(move, player)
+        score = minimax(node, get_opponent(player), alpha, beta)
+        node.move(move, ' ')
+        if player == 'O':
+            if score > alpha:
+                alpha = score
+            if alpha >= beta:
+                return beta
+        else:
+            if score < beta:
+                beta = score
+            if beta <= alpha:
+                return alpha
+    if player == 'O':
+        return alpha
+    else:
+        return beta
+
+def computer_move2(board, player):
+    best = -2
+    choices = []
+    
+    if len(board.legal_moves()) == 9:
+        return random.choice([0,2,6,8])
+    for move in board.legal_moves():
+        board.move(move, player)
+        score = minimax(board, get_opponent(player), -2, 2)
+        board.move(move, ' ')
+        print("Move: ",move+1," has a score of: ",score)
+        if score > best:
+            best = score
+            choices = [move]
+        elif score == best:
+            choices.append(move)
+    choice = random.choice(choices)
+    print("[+] Selected move: ",choice+1)
+    return choice
+
 def computer_play():
-    """ Play Tic-Tac-Toe against a (stupid) computer"""
+    """ Play Tic-Tac-Toe against a (perfect) computer"""
     board = Board()
     board.output()
     player1 = input("What are you called? ")
@@ -84,7 +133,8 @@ def computer_play():
             move = player_move(board, '{}\'s turn'.format(player1))
             board.move(move, player)
         else:
-            move = computer_move(board, player)
+            move = computer_move2(board, player)
+            print("Computer will play at square: {}".format(move))
             board.move(move, player)
         board.output()
         player = get_opponent(player)
